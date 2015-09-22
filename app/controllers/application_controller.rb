68157a9550dc9e409353549314c9bd73e92cece9
Ankit Samarthya <ankitsamar@outlook.com>
2015-09-23 01:19:37 +0530

    time zones

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,19 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  around_filter :user_time_zone, if: :current_user
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
 
     def configure_permitted_parameters
-      devise_parameter_sanitizer.for(:sign_up) << :name << :role << :avatar << :about 
-      devise_parameter_sanitizer.for(:account_update) << :name << :role << :avatar << :about
+      devise_parameter_sanitizer.for(:sign_up) << :name << :role << :avatar << :about << :time_zone
+      devise_parameter_sanitizer.for(:account_update) << :name << :role << :avatar << :about << :time_zone
+    end
+
+    def user_time_zone(&block)
+    	Time.use_zone(current_user.time_zone, &block)
     end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150922140512) do
+ActiveRecord::Schema.define(version: 20150922193403) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,6 +74,7 @@ ActiveRecord::Schema.define(version: 20150922140512) do
     t.string   "avatar"
     t.text     "about"
     t.datetime "updated_at"
+    t.string   "time_zone"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
