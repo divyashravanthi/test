9758de173f6830902468cfd697033786a904ee2d
Ankit Samarthya <ankitsamar@outlook.com>
2015-09-23 01:36:11 +0530

    localtime

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  around_filter :user_time_zone, if: :current_user
+  # around_filter :user_time_zone, if: :current_user
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
